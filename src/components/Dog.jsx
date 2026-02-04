16707693d1138ed17e2d2e33b043571bca951f4f
Siddhant Yadav <sidhantyadav1999@gmail.com>
2026-02-04 22:47:43 +0530

    feat: Display 3D dog model with lighting and initial camera setup

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,15 +1,17 @@
 import { useThree } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, useGLTF } from "@react-three/drei"
 const Dog = () => {
-    const { camera } = useThree()
-    console.log(camera.position)
+    const model = useGLTF("/dog.drc.glb")
+
+    useThree(({ camera }) => {
+        camera.position.z = 0.7
+    })
     return (
         <>
-            <mesh>
-                <meshBasicMaterial color={"red"} />
-                <boxGeometry args={[1, 1, 1]} />
-            </mesh>
-            <OrbitControls />
+            <primitive object={model.scene} position={[0.2, -0.5, 0]} rotation={[0, -5.7, 0]} />
+            <directionalLight position={[-10, 10, 20]} intensity={20} color={"#D4AF37"} />
+            <directionalLight position={[10, 10, -20]} intensity={20} color={"#D4AF37"} />
+            {/* <OrbitControls /> */}
         </>
     )
 }
