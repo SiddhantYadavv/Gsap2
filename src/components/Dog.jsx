967b47b190db2f4fc425d3f45f160c430f7824d9
Siddhant Yadav <sidhantyadav1999@gmail.com>
2026-02-06 22:42:46 +0530

    feat: apply normal map to dog model and reduce directional light intensity

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,16 +1,27 @@
 import { useThree } from "@react-three/fiber"
-import { OrbitControls, useGLTF } from "@react-three/drei"
+import * as THREE from "three"
+import { OrbitControls, useGLTF, useTexture } from "@react-three/drei"
 const Dog = () => {
     const model = useGLTF("/dog.drc.glb")
 
     useThree(({ camera }) => {
         camera.position.z = 0.7
     })
+    const texture = useTexture("/dog_normals.jpg")
+
+    model.scene.traverse((child) => {
+        if (child.name.includes("DOG")) {
+            child.material = new THREE.MeshStandardMaterial({
+                normalMap: texture,
+                // normalScale: [1, 1],
+            })
+        }
+    })
     return (
         <>
             <primitive object={model.scene} position={[0.2, -0.5, 0]} rotation={[0, -5.7, 0]} />
-            <directionalLight position={[-10, 10, 20]} intensity={20} color={"#D4AF37"} />
-            <directionalLight position={[10, 10, -20]} intensity={20} color={"#D4AF37"} />
+            <directionalLight position={[-10, 10, 20]} intensity={10} color={"#D4AF37"} />
+            <directionalLight position={[10, 10, -20]} intensity={10} color={"#D4AF37"} />
             {/* <OrbitControls /> */}
         </>
     )
